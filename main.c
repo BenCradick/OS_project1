a9f8f3856c2fbcfcebe44045d2e4abd13edfd68e
bencradick <ben.cradick@gmail.com>
2019-02-05 10:17:34 -0600

    File is opening, began code for reading contents.

--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <limits.h>
 
 int main(int argc, char **argv) {
     char *inputFile;
     char *outputFile;
     int c; // used to hold integer return value of getopt
+    int childCount; // number of children to be generated, also first line in inputFile.
+
+    char line[LINE_MAX];
 
     inputFile = "input.dat";
     outputFile = "output.dat";
+
+    FILE *inputStream ;
 
     opterr = 0;
 
@@ -34,10 +40,10 @@ int main(int argc, char **argv) {
 
             case '?':
                 if (optopt == 'i')
-                    perror("Option -i requires an argument.");
+                   fprintf(stderr, "Option -i requires an argument");
 
                 else if (optopt == 'o')
-                    perror("Option -o requires an argument.");
+                    fprintf(stderr, "Option -o requires an argument.");
 
                 else if (isprint(optopt) == 1)
                     fprintf(stderr, "Unknown option `-%c'.\n", optopt);
@@ -48,10 +54,31 @@ int main(int argc, char **argv) {
                 return 1;
 
             default:
+                fprintf(stderr, "Unknown command line argument error occurred. \n");
+                printf("Argument error: Use command cradick.1 -h  for help");
                 abort();
 
         }
     }
+
+
+    inputStream = fopen(inputFile, "r");
+
+    if(inputStream == NULL){
+        perror(inputFile);
+        return -1;
+    }
+
+    if(fgets(line, LINE_MAX, inputStream) == NULL){
+        fprintf(stderr, "File %s contains no text", inputFile);
+        fclose(inputStream);
+        return 2;
+    }
     
-    return 0;
+
+
+
+
+
+    return (0);
 }
