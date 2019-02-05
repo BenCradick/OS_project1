ccd9ff2b1d8b30e97a9e688e9cc378a2a37f6ed1
bencradick <ben.cradick@gmail.com>
2019-02-04 19:08:45 -0600

    Finished argument parsing protocol.

--- main.c
+++ main.c
@@ -2,20 +2,26 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char **argv) {
     char *inputFile;
     char *outputFile;
     int c; // used to hold integer return value of getopt
 
+    inputFile = "input.dat";
+    outputFile = "output.dat";
+
+    opterr = 0;
+
     while((c = getopt(argc, argv, "hi:o:")) != -1 ){
-        switch(c){
+        switch(c) {
             case 'h':
                 printf("%s\n\t%s\n\t%s\n\t%s",
-                    "The available options for this program are:",
-                    "-h: Display the help (this) page",
-                    "-i: Specify an input file. ex: cradick.1 -i [file]",
-                    "-o: Specify an output file. ex: cradick.1 -o [filename]");
+                       "The available options for this program are:",
+                       "-h: Display the help (this) page",
+                       "-i: Specify an input file. ex: cradick.1 -i [file]",
+                       "-o: Specify an output file. ex: cradick.1 -o [filename]");
                 break;
 
             case 'i' :
@@ -24,17 +30,21 @@ int main(int argc, char **argv) {
 
             case 'o' :
                 outputFile = optarg;
+                break;
 
             case '?':
                 if (optopt == 'i')
-                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-                else if (isprint (optopt))
-                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                    perror("Option -i requires an argument.");
+
+                else if (optopt == 'o')
+                    perror("Option -o requires an argument.");
+
+                else if (isprint(optopt) == 1)
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
 
                 else
-                    fprintf (stderr,
-                             "Unknown option character `\\x%x'.\n",
-                             optopt);
+                    fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+
                 return 1;
 
             default:
@@ -42,5 +52,6 @@ int main(int argc, char **argv) {
 
         }
     }
+    
     return 0;
 }
